82e34e87586c62662d983fb08bb56413beafe32e
Migue <miguelpuig@gmail.com>
2021-08-21 18:34:39 +0100

    Add terms

--- utils/getRoutes.js
+++ utils/getRoutes.js
@@ -1,11 +1,13 @@
-module.exports =  function getAppRoutes() {
+module.exports = function getAppRoutes() {
 
-    return [
-      '/home/en-gb',
-      '/home/de-de',
-      '/terms/en-gb',
-      '/terms/de-de',
-      '/contact/en-gb',
-      '/contact/de-de'
-    ]
+  return [
+    '/home/en-gb',
+    '/home/de-de',
+    '/terms/en-gb',
+    '/terms-survey/en-gb',
+    '/survey-gdpr-statement/en-gb',
+    '/terms/de-de',
+    '/contact/en-gb',
+    '/contact/de-de'
+  ]
 };
